28acec6b8bc5b8ec6848ddd03e2d886781615745
SaiPranav-Y <saipranav.spy@gmail.com>
2022-05-19 19:07:37 +0530

    modified basicprogram.java

--- Basicprogram.java
+++ Basicprogram.java
@@ -12,6 +12,10 @@ public class Basicprogram {
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
         System.out.println("Int: " + i);
+        System.out.println("------------------");
+        String s1 = scan.nextLine();
+        int x = scan.nextInt();
+        System.out.printf("%-15s%03d%n", s1, x);// Here, we can use printf function to format the spacing and padding in a line,
 
         scan.close();
     }
